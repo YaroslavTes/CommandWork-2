266bde5874b8f8256984e8646a6b98b7e0064ecd
YaroslavTes <yaroslav.tesliyk@gmail.com>
2022-06-25 00:13:23 +0300

    Скрипт для меню

---
+++ menu/js/menu.js
@@ -0,0 +1,12 @@
+var button = document.querySelector('.signBtn');
+console.log("button");
+
+button.onclick = function () {
+
+    alert("Регистрация")
+
+    setTimeout(function () {
+        location.reload();
+    }, 500)
+
+}
