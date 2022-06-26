3e6ff5ffc338a982a367b200e02cbf5a630dfb45
YaroslavTes <yaroslav.tesliyk@gmail.com>
2022-06-26 20:03:35 +0300

    script slider

--- slaider/js/slider.js
+++ slaider/js/slider.js
@@ -5,7 +5,25 @@ $(function() {
 		dots: true,
 		autoplay: true,
 		autoplaySpeed: 2000,
-		speed: 2000
+		speed: 2000,
+		responsive: [
+			{
+			  breakpoint: 767,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			  }
+			},
+			{
+			  breakpoint: 480,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				arrows: false,
+				dots: true
+			  }
+			}
+	]
+	
 	})
-
 })
