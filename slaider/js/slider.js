e024bc8d12c0716875524c9dc956e084fcbf336d
YaroslavTes <yaroslav.tesliyk@gmail.com>
2022-06-26 22:32:02 +0300

    script slider

--- slaider/js/slider.js
+++ slaider/js/slider.js
@@ -3,39 +3,33 @@ $(function() {
 	$(".rev-slider").slick({
 		dots: true,
 		autoplay: true,
-		autoplaySpeed: 2000,
-		speed: 2000,
-		slidesToScroll: 1,
-		slidesToShow: 1,
+		autoplaySpeed: 3500,
+		speed: 300,
 		responsive: [
-			{
-			  breakpoint: 1024,
-			  settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				infinite: true,
-				dots: true,
-				arrow: false
-			  }
-			},
-			{
-			  breakpoint: 600,
-			  settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				dots: true,
-				arrow: false
-			  }
-			},
-			{
-			  breakpoint: 480,
-			  settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				dots: true,
-				arrow: false
-			  }
+		  {
+			breakpoint: 1024,
+			settings: {
+			  slidesToShow: 1,
+			  slidesToScroll: 1,
+			  infinite: true,
+			  dots: true,
+			  arrows: false
 			}
-		  ]
-	})
+		  },
+		  {
+			breakpoint: 600,
+			settings: {
+			  slidesToShow: 1,
+			  slidesToScroll: 1
+			}
+		  },
+		  {
+			breakpoint: 480,
+			settings: {
+			  slidesToShow: 1,
+			  slidesToScroll: 1
+			}
+		  }
+		]
+	});
 })
